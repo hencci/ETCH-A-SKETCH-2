e12c7414d0d3cfb75afc3b08e0f8ca04500627f7
hencci <hencci20@gmail.com>
2024-11-28 16:57:46 +0100

    Corrected the for loop condition

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let grids = 16;
 function makeGrids(squares) {
     const containerWidth = container.clientWidth;
     const squareSize = containerWidth/squares;
-    for (let i =1; i <(squares * squares); i++) {
+    for (let i =1; i <= (squares * squares); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `${squareSize}px`;
