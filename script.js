287db35894b9029bfe0dde5e4dbb5b54f25e2766
hencci <hencci20@gmail.com>
2024-11-28 16:23:39 +0100

    Set width and height of the squares

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ function makeGrids(squares) {
     for (let i =1; i <(squares * squares); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
     }
 }
