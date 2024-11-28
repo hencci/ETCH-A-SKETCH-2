dab75dab0858be8927de15d619ec9d9a29751e85
hencci <hencci20@gmail.com>
2024-11-28 16:17:05 +0100

    Create div and loop throught to get required number of squares

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let grids = 16;
 function makeGrids(squares) {
     const containerWidth = container.clientWidth;
     const squareSize = containerWidth/squares;
+    for (let i =1; i <(squares * squares); i++) {
+        const square = document.createElement("div");
+    }
 }
